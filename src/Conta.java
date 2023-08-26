739ff100c7c4ec3d76c051af5d54eda6987700e0
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-08-25 22:21:40 -0300

    criando a classe conta e seus getters e setters e construindo o objeto na contaTerminal

---
+++ src/Conta.java
@@ -0,0 +1,36 @@
+public class Conta {
+    
+    String nomeCliente;
+    String agencia;
+    int numeroConta;
+    double saldo;
+
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+
+}
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // trazendo a classe conta
+        Conta createConta = new Conta();
+
+        // criação da conta
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome = sc.next();
+        createConta.setNomeCliente(nome);
+
+        System.out.println("Por favor, digite o número da agencia: ");
+        String agencia = sc.next();
+        createConta.setAgencia(agencia);
+
+        System.out.println("Por favor, digite o número da conta: ");
+        int conta = sc.nextInt();
+        createConta.setNumeroConta(conta);
+
+        System.out.println("Por favor, digite o valor a ser depositado na sua conta: ");
+        double saldo = sc.nextDouble();
+        createConta.setSaldo(saldo);
+        
+        sc.close();
+
+        System.out.println("***********************************");
+        System.out.println("******** Criando sua conta ********");
+        System.out.println("***********************************");
+
+        System.out.println("-- Parabéns, sua conta foi criada.");
+
+        System.out.println(String.format("Ol\u00E1 %s, obrigado por criar uma conta em nosso banco, sua ag\u00EAncia \u00E9 %s, conta %s e seu saldo %s j\u00E1 est\u00E1 dispon\u00EDvel para saque.", nome, agencia, conta, saldo));
+
     }
+    
 }
